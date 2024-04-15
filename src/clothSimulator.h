36766762fb76c7a3c385be444d5d937818577cc4
Eugene Kang <eugene.k07@berkeley.edu>
2024-04-15 15:00:34 -0700

    vertical movement

--- src/clothSimulator.h
+++ src/clothSimulator.h
@@ -59,7 +59,7 @@ private:
   int frames_per_sec = 90;
   int simulation_steps = 30;
 
-  CGL::Vector3D gravity = CGL::Vector3D(0, -9.8, 0);
+  CGL::Vector3D gravity = CGL::Vector3D(0, 0, 4.5);
   nanogui::Color color = nanogui::Color(1.0f, 1.0f, 1.0f, 1.0f);
 
   Cloth *cloth;
