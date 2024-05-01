4389a81854f8c6c81a78f692a2ecef7add67e72a
Han <chinzonghan3101@berkeley.edu>
2024-04-30 17:47:06 -0700

    updated default values

--- src/clothSimulator.h
+++ src/clothSimulator.h
@@ -57,8 +57,8 @@ private:
 
   // Default simulation values
 
-  int frames_per_sec = 90;
-  int simulation_steps = 30;
+  int frames_per_sec = 30;
+  int simulation_steps = 5;
 
   CGL::Vector3D gravity = CGL::Vector3D(0, 0, 4.5);
   nanogui::Color color = nanogui::Color(1.0f, 1.0f, 1.0f, 1.0f);
